797fa3ac2e7fa1f9a6f036454e74728344cf224f
srisreedhar <srisreedhar@live.com>
2026-06-08 17:57:39 +0530

    added few more commands

--- SnowFlake/SF_Lab_CopyCommands.sql
+++ SnowFlake/SF_Lab_CopyCommands.sql
@@ -155,4 +155,99 @@ SELECT * FROM csvfilesdb.rawdata_schema.ORDERS_EX;
 
 
 -- drop database
-drop database csvfilesdb;
+-- drop database csvfilesdb;
+
+
+-- Defining Size_Limit for a file in stage
+
+
+CREATE OR REPLACE STAGE csvfilesdb.external_stages.aws_files_stage
+    url='s3://bucketsnowflakes4'
+    size_limit = 5000000; -- 5 MB 
+
+-- only the files/data till 5MB will be loaded into the table, 
+-- rest will be skipped and error will be logged in the load history.
+
+
+
+-- RETURN_FAILED_ONLY
+
+
+
+-- This option is used to load only the failed records using the COPY command.
+-- This option is used in conjunction with the ON_ERROR option to specify how to handle errors during the load process.
+-- When the ON_ERROR option is set to 'CONTINUE', the COPY command will continue to load the data even if there are errors in the file. If the RETURN_FAILED_ONLY option is set to true, only the records that failed to load will be returned in the result set.
+
+COPY INTO csvfilesdb.rawdata_schema.ORDERS_EX
+    FROM @csvfilesdb.external_stages.aws_files_stage
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    RETURN_FAILED_ONLY = TRUE;
+
+
+-- Truncate Columns
+
+
+COPY INTO COPY_DB.PUBLIC.ORDERS
+    FROM @aws_stage_copy
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*'
+    TRUNCATECOLUMNS = true; 
+    
+
+-- specifies whether to truncate the string values in the columns that exceed the defined column width. If set to true, the string values will be truncated to fit the column width, and a warning will be logged in the load history. If set to false, an error will be raised if any string value exceeds the defined column width, and the load process will be aborted.
+-- when set ,     TRUNCATECOLUMNS = true, the string values that exceed the defined column width will be truncated to 
+-- fit the column width of the Target tables
+
+
+
+
+
+-- FORCE Load
+-- forces to load data from the same source and from same file again  resulting in duplicates
+
+
+CREATE OR REPLACE TABLE  csvfilesdb.rawdata_schema.ORDERS (
+    ORDER_ID VARCHAR(30),
+    AMOUNT VARCHAR(30),
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(30),
+    SUBCATEGORY VARCHAR(30));
+
+    
+-- S3 stage object
+
+CREATE OR REPLACE STAGE csvfilesdb.external_stages.aws_stage_copy
+    url='s3://snowflakebucket-copyoption/size/';
+
+list @csvfilesdb.external_stages.aws_stage_copy;
+
+
+-- copy command
+COPY INTO csvfilesdb.rawdata_schema.ORDERS 
+    FROM @aws_stage_copy
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*';
+
+
+truncate table csvfilesdb.rawdata_schema.ORDERS;
+-- run one more time
+
+COPY INTO csvfilesdb.rawdata_schema.ORDERS 
+    FROM @aws_stage_copy
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*';
+
+select count(*) from csvfilesdb.rawdata_schema.ORDERS;
+-- now force run
+
+COPY INTO csvfilesdb.rawdata_schema.ORDERS 
+    FROM @aws_stage_copy
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*'
+    force = TRUE;
+
+select count(*) from csvfilesdb.rawdata_schema.ORDERS;
+
+
+truncate table csvfilesdb.rawdata_schema.ORDERS;
